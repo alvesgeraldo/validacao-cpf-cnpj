4620a143ee544787b9d68ae11fb43d1a8ceffff5
Geraldo Alves <92646470+alvesgeraldo@users.noreply.github.com>
2022-09-23 17:21:59 -0300

    Incluido a função de gerar uma numeração CPF válida

---
+++ geradorCPF.js
@@ -0,0 +1,77 @@
+$(document).ready(() => {
+
+  function geraPrimeiroDigito(array){
+    
+    let n = 10;
+    let soma = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      let mult = array[i] * n;
+      soma += mult;
+      n--;      
+    }
+
+    let resto = (soma * 10)%11;
+
+    if(resto == 10){
+      resto = 0;
+    }
+
+    return resto;
+
+  }
+
+  function geraSegundoDigito(array){
+
+    let n = 11;
+    let soma = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      let mult = array[i] * n;
+      soma += mult;
+      n--;      
+    }
+
+    let resto = (soma * 10)%11;
+
+    if(resto == 10){
+      resto = 0;
+    }
+
+    return resto;
+
+  }
+
+  
+  
+  $('#btnGerador').on('click', () => {
+
+      let novoCPF = [];
+
+      for (let i = 0; i < 9; i++) {
+        let novoDigito = Math.floor(Math.random() * 10);
+        novoCPF.push(novoDigito);
+      }
+
+      let primeiroDigitoVerificador = geraPrimeiroDigito(novoCPF);
+      novoCPF.push(primeiroDigitoVerificador);
+
+      let segundoDigitoVerificador = geraSegundoDigito(novoCPF);
+      novoCPF.push(segundoDigitoVerificador);
+
+      $('#cpf_cnpj').val(novoCPF.join(''))
+
+  }) 
+  
+  $('#btnCopiar').on('click', () => {
+
+    let txt = document.getElementById('cpf_cnpj');
+    txt.select();
+    document.execCommand("copy");
+
+  })
+
+  
+
+
+})
