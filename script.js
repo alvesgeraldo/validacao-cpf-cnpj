03fb8bc151af0db693f22edf857f893bebf0e357
Geraldo Alves <92646470+alvesgeraldo@users.noreply.github.com>
2022-09-23 14:02:32 -0300

    Finalizando mensagem de validacao e informações no index

--- script.js
+++ script.js
@@ -78,112 +78,136 @@ $(document).ready(() => {
 
   }
 
+  // Validação CNPJ
+  // Verifica primeiro digito
+  function verificaPrimeiroDigitoCNPJ(array){
+
+    let soma = 0;
+    let n = 5;
+    let resto = 0;
+    let primeiroDigito = 0;
+
+    for (let i = 0; i < array.slice(0, -2).length; i++) {
+      if(n == 1){
+        n = 9;
+      }
+      let mult = array[i] * n;
+      soma += mult;
+      n--;
+    }
+
+    resto = soma % 11;
+
+    if(resto == 0 || resto == 1){
+      primeiroDigito = 0;
+    } else {
+      primeiroDigito = 11- resto;
+    }
+
+    return primeiroDigito;
+
+  }
+
+  // Verifica segundo dígito
+  function verificaSegundoDigitoCNPJ(array){
+
+    let soma = 0;
+    let n = 6;
+    let resto = 0;
+    let segundoDigito = 0;
+
+    for (let i = 0; i < array.slice(0, -1).length; i++) {
+      if(n == 1){
+        n = 9;
+      }
+      let mult = array[i] * n;
+      soma += mult;
+      n--;
+    }
+
+    resto = soma % 11;
+
+    if(resto == 0 || resto == 1){
+      segundoDigito = 0;
+    } else {
+      segundoDigito = 11- resto;
+    }
+
+    return segundoDigito;
+
+  }
+
+  // Função principal para validação CNPJ
+  function validacaoCNPJ(array){
+
+    // Verifica se não é sequencia de 0
+    if(array.join('') == '00000000000000'){
+      return false;
+    }
+
+    // Retorno da verificação do primeiro e segundo dígito
+    let primeiroDigito = verificaPrimeiroDigitoCNPJ(array);
+    let segundoDigito = verificaSegundoDigitoCNPJ(array);
+
+    // Verifica se é diferente dos dígitos do CNPJ digitado pelo usuário
+    if(primeiroDigito != array[12] && segundoDigito != array[13]){
+      return false;
+    } 
+
+    return true;
+
+  }
+
   $('#btnForm').on('click', () => {
 
     let cpf_cnpjInput = $('#cpf_cnpj').val().replace(/[^0-9]/g, '').split('');
     
-    if(cpf_cnpjInput.length == 11){
+    switch (cpf_cnpjInput.length) {
+      case 11:
+        
+        $('#cpf_cnpj').mask('999.999.999-99');
+        let resultadoValidacaoCPF = validacaoCPF(cpf_cnpjInput);
 
-      $('#cpf_cnpj').mask('999.999.999-99');
-      let resultadoValidacaoCPF = validacaoCPF(cpf_cnpjInput);
-
-      if(resultadoValidacaoCPF){
-        $('$res').addClass('text-success');
-        $('$res').addClass('O CPF');
-
-      }
-
+        if(resultadoValidacaoCPF){
+          $('#res').removeClass('text-danger');
+          $('#res').addClass('text-success');
+          $('#res').html('<strong>O CPF digitado é válido</strong>');
+        } else {
+          $('#res').removeClass('text-success');
+          $('#res').addClass('text-danger');
+          $('#res').html('<strong>O CPF digitado é inválido</strong>');
+        }
+        break;
+    
+      case 14:
+        
+      $('#cpf_cnpj').mask('99.999.999/9999-99');
+        let resultadoValidacaoCNPJ = validacaoCNPJ(cpf_cnpjInput);
+        
+        if(resultadoValidacaoCNPJ){
+          $('#res').removeClass('text-danger');
+          $('#res').addClass('text-success');
+          $('#res').html('<strong>O CNPJ digitado é válido</strong>');
+        } else {
+          $('#res').removeClass('text-success');
+          $('#res').addClass('text-danger');
+          $('#res').html('<strong>O CNPJ digitado é inválido</strong>');
+        }
+        break;
     }
+
+    $('#cpf_cnpj').focus(() => {
+
+      $('#res').html('');
+      $('#cpf_cnpj').unmask();
+      $('#cpf_cnpj').val('');
+  
+    })
+    
     
     /*  
-    
 
-    
-    
-    if(!resultadoValidacaoCPF){
-      $(e.target).addClass('is-invalid');
-      $('#msg').html('CPF inválido');
-    } else {
-      $(e.target).removeClass('is-invalid');
-      $('#msg').html('');
-    }
 
-    let resultadoCPFCadastrado = verificaCPFCadastrados(cpfInput.join(''));
-
-    console.log(resultadoCPFCadastrado);
-
-    function verificaPrimeiroDigito(array){
-
-      let soma = 0;
-      let n = 5;
-      let resto = 0;
-      let primeiroDigito = 0;
-  
-      for (let i = 0; i < array.slice(0, -2).length; i++) {
-        if(n == 1){
-          n = 9;
-        }
-        let mult = array[i] * n;
-        soma += mult;
-        n--;
-      }
-  
-      resto = soma % 11;
-  
-      if(resto == 0 || resto == 1){
-        primeiroDigito = 0;
-      } else {
-        primeiroDigito = 11- resto;
-      }
-  
-      return primeiroDigito;
-  
-    }
-  
-    function verificaSegundoDigito(array){
-  
-      let soma = 0;
-      let n = 6;
-      let resto = 0;
-      let segundoDigito = 0;
-  
-      for (let i = 0; i < array.slice(0, -1).length; i++) {
-        if(n == 1){
-          n = 9;
-        }
-        let mult = array[i] * n;
-        soma += mult;
-        n--;
-      }
-  
-      resto = soma % 11;
-  
-      if(resto == 0 || resto == 1){
-        segundoDigito = 0;
-      } else {
-        segundoDigito = 11- resto;
-      }
-  
-      return segundoDigito;
-  
-    }
-  
-    function validacaoCNPJ(array){
-  
-      if(array.join('') == '00000000000000'){
-        return false;
-      }
-  
-      let primeiroDigito = verificaPrimeiroDigito(array);
-      let segundoDigito = verificaSegundoDigito(array);
-  
-      if(primeiroDigito != array[12] && segundoDigito != array[13]){
-        return false;
-      } 
-  
-      return true;
-  
-    }
   
     $('#cpf').blur((e) => {
   
